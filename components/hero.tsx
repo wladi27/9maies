bbc48f7e7ca343a72252a4a5f9f6819c2666649f
wladi27 <figuerawladimir@gmail.com>
2026-01-13 17:18:42 -0400

    detalles eventos img

--- components/hero.tsx
+++ components/hero.tsx
@@ -82,7 +82,7 @@ export function Hero() {
 
 function CarouselInner() {
   const eventImages = [
-    's1.jpeg','s2.png','s3.png','s4.png','s5.png','s6.png','s7.png','s8.png','s9.png','s10.png'
+    's1.jpeg','s2.png','s3.png','s4.png','s5.png','s6.png','s7.png','s8.png','s9.png','s10.png', 's11.png','s12.png','s13.png','s14.png','s15.png','s16.png','s17.png','s18.png','s19.png','s20.png', 's21.png','s22.png','s23.png','s24.png','s25.png','s26.png','s27.png','s28.png'
   ]
 
   const [currentSlide, setCurrentSlide] = useState(0)
